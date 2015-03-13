2e0790bd1dd2b2f94106ec057255ad4a365242af
Nicolas <nicolas.rabault@inria.fr>
2015-03-13 17:04:29 +0100

    update setup script
    WARNING : DocumentRoot config need to be :
    DocumentRoot /var/www/web
    
    and websudoer.sh need to have executable right
    
    these information are not mentioned into the root README

--- poppy/setup.sh
+++ poppy/setup.sh
@@ -9,23 +9,29 @@ then
 fi
 
 # Install needed apps
+apt-get update
 apt-get -y install apache2 php5 libapache2-mod-php5 network-manager \
-avahi-daemon libnss-mdns
+avahi-daemon libnss-mdns subversion
 
 # Sudo configuration for the websudoer app
-echo "www-data ALL=(ALL) NOPASSWD: /usr/local/websudoer/websudoer.sh" >> /etc/sudoers
+echo "www-data ALL=(ALL) NOPASSWD: /usr/local/robot/websudoer/websudoer.sh" >> /etc/sudoers
 
 # Apache configuration
-sed -i.bkp '/VirtualHost>/i #added lines\n\t<Directory />\n\t\tOrder deny,allow\n\t\tAllow from all\n\t</Directory>' /etc/apache2/sites-enabled/000-default.conf
-sed -i 's/\/var\/www\/html/\/var\/www/g' /etc/apache2/sites-enabled/000-default.conf
+if ! grep -q added /etc/apache2/sites-enabled/000-default.conf
+then
+    sed -i.bkp '/VirtualHost>/i #added lines\n\t<Directory \/web\/>\n\t\tOrder deny,allow\n\t\tAllow from all\n\t</Directory>' /etc/apache2/sites-enabled/000-default.conf
+    sed -i 's/\/var\/www\/html/\/var\/www\/web/g' /etc/apache2/sites-enabled/000-default.conf
+fi
 
 # Add scripts
-mkdir /usr/local/proteo
-mkdir /usr/local/proteo/websudoer
+mkdir /usr/local/robot
+mkdir /usr/local/robot/websudoer
+rm /usr/local/robot/websudoer/websudoer.sh
+mkdir /var/www/web
 
-# After the post off the first version of this script on github, change it to download scripts
-cp websudoer.sh /usr/local/proteo/websudoer/
+# Download sources
+wget -P  /usr/local/robot/websudoer https://raw.githubusercontent.com/MakingBot/webapp/master/websudoer/websudoer.sh
+chmod +x /usr/local/robot/websudoer/websudoer.sh
+svn checkout https://github.com/MakingBot/webapp/trunk/web /var/www/web
 
-rm -rf /var/www/*
-
-cp -r Web/* /var/www/
+echo Please reboot to apply changes
